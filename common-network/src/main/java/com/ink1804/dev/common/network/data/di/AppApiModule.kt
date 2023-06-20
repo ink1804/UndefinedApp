89eb47fcb6cdfca2ca15e5148b2757affb017ae7
s.shisheia <s.shisheia@prequelapp.com>
2023-06-20 11:24:27 +0400

    replced retrofit with ktor

--- common-network/src/main/java/com/ink1804/dev/common/network/data/di/AppApiModule.kt
+++ common-network/src/main/java/com/ink1804/dev/common/network/data/di/AppApiModule.kt
@@ -1,13 +1,6 @@
 package com.ink1804.dev.common.network.data.di
 
-import com.ink1804.dev.common.network.CommonNetworkConfig
-import com.ink1804.dev.common.network.data.api.AppApi
 import dagger.Module
-import dagger.Provides
-import javax.inject.Named
-import okhttp3.OkHttpClient
-import retrofit2.Converter
-import retrofit2.Retrofit
 
 @Module(
     includes = [
@@ -16,21 +9,4 @@ import retrofit2.Retrofit
 )
 object AppApiModule {
 
-    private const val APP_API_DAGGER_NAME = "app_api_dagger_name"
-
-    @Provides
-    @Named(APP_API_DAGGER_NAME)
-    fun provideRetrofit(
-        okHttpClient: OkHttpClient,
-        commonNetworkConfig: CommonNetworkConfig,
-        converterFactory: Converter.Factory
-    ): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(commonNetworkConfig.appApiBaseUrl)
-            .client(okHttpClient)
-            .addConverterFactory(converterFactory)
-            .build()
-
-    @Named(APP_API_DAGGER_NAME)
-    fun provideApi(retrofit: Retrofit): AppApi = retrofit.create(AppApi::class.java)
 }
